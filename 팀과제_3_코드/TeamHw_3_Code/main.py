f350d307e4bd4a9b7b5669ed85ef11f9e091e8d8
Rabtm <minseok0412@naver.com>
2022-11-01 22:12:29 +0900

    [22.11.01]Feat: Programming Language

--- 팀과제_3_코드/TeamHw_3_Code/main.py
+++ 팀과제_3_코드/TeamHw_3_Code/main.py
@@ -16,6 +16,13 @@ def result():
       result['Gender'] = request.form.get('Gender')
       # 학과
       # 프로그래밍 언어 -> hint) ','.join(list명)을 사용하면 list 안에 있는 항목들이 ','로 나누어져 출력됨.
+      result['Programing Languague'] = " "
+      lst = ["Python", "Java", "HTML", "C++"]
+      lst2 = []
+      for x in lst:
+         if request.form.get(x):
+            lst2.append(x)
+      result['Programing Languague'] = ', '.join(lst2)
 
       return render_template("result.html",result = result)
 
